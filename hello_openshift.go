1b113e6a6196106f5da713c278d9f80520c755dd
ShigeruKishikawa <33087289+ShigeruKishikawa@users.noreply.github.com>
2021-04-14 11:33:28 +0900

    Update hello_openshift.go

--- hello_openshift.go
+++ hello_openshift.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift!"
+		response = "Hello OpenShift! Apr14, 2021"
 	}
 
 	fmt.Fprintln(w, response)
